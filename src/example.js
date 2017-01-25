78289a7a732c682eab29ce5f3827feb9d4bf62bc
Nicholas Rees <nicholas.rees@live.co.uk>
2017-01-25 20:13:07 +0000

    Example function

---
+++ src/example.js
@@ -0,0 +1,19 @@
+$(function() {
+	/* ********************************* *
+	 * <h1>example</h1>
+	 *
+	 * Example text
+	 * 
+	 *
+	 * @author  Nicholas Rees
+	 * @version 1.0
+	 * @since   25-01-2017
+	 *
+	 * @param params - this is the param
+	 * 
+	 * @return
+	 * ********************************* */
+	function _.example(params) {
+
+	}
+});
